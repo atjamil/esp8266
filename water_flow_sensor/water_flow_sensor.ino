ea562726c1340f789bb7e12bfd07158233a8d55f
atjamil <45707191+atjamil@users.noreply.github.com>
2021-09-02 18:25:49 +0800

    Update water_flow_sensor.ino

--- water_flow_sensor/water_flow_sensor.ino
+++ water_flow_sensor/water_flow_sensor.ino
@@ -1,159 +1,178 @@
 /*
- Soil moisture sensor project
+ Water flow sensor project
  Connected to ESP8266
  
  Data send to Thinger.io for visualisation
  Send notification to IFTTT when treshold reach
 
-     < 500 is too wet
-    500-750 is the target range
-    > 750 is dry enough to be watered
-
-    Range: 315-1024
-
     BOARD: ESP8266-ESP12E
-    BOARD-No2
+    BOARD-No1
     
     PIN
-    SENSOR-->ESP(2)
-    GND(-)-->GND
-    VCC(+)-->D7
-    DO-->
-    AO-->A0
+    SENSOR-->ESP(1)
+    Black(GND)-->G(Pin beside 3V)//Working
+    Red(VCC)-->VIN
+    Yellow-->D4 (Working)
+*/
 
- 
- */
-#define _DISABLE_TLS_ //This will solve problem to connect to thinger.io server
-#define _DEBUG_
-#include <ThingerESP8266.h>
-#include "AnotherIFTTTWebhook.h"
+#define _DISABLE_TLS_ //This will solve problem to connect to thinger.io server by disabling secure TLS/SSL
+#define _DEBUG_ //For debugging
+#include <ThingerESP8266.h> //For thinger.io (Please add library in Arduino IDE
+#include "AnotherIFTTTWebhook.h" //For IFTTT: webhook please add this file in the same folder
 #include <ESP8266WiFi.h>
 
-//Thinger.io
+//Thinger.io (You must have account with Thinger.io)
 #define USERNAME "atjamil"
-#define DEVICE_ID "esp8266_2"
+#define DEVICE_ID "esp8266_1"
 #define DEVICE_CREDENTIAL "2014polar"
 
 // Set IFTTT Webhooks event name and key
 #define IFTTT_Key "dHXvG5qbGylRXe2PjHX8Py"
-#define IFTTT_Event "ESP8266_SOIL_MOISTURE"
+#define IFTTT_Event "ESP8266_notification_test"
 
 //Local wifi setting
-#define SSID "atjamil@unifi"
-#define SSID_PASSWORD "2014polar"
+#define SSID "YOUR_SSID"
+#define SSID_PASSWORD "PASSWORD"
 
-/* Change these values based on your calibration values */
-#define soilWet 500   // Define max value we consider soil 'wet'
-#define soilDry 750   // Define min value we consider soil 'dry'
-
-//Soil moisture sensor attached to esp8266(data-yellow)
-#define SENSORPIN  A0
-#define SENSORPOWER D7
+//Water flow sensor attached to esp8266(data-yellow)
+#define SENSOR  D4 //working
 
 //Connecting to thinger.io
 ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
 
-char buff[6];
+long currentMillis = 0;
+long previousMillis = 0;
+int interval = 1000;
+
+//boolean ledState = LOW;
+float calibrationFactor = 4.5;
+
+volatile byte pulseCount;
+byte pulse1Sec = 0;
+
+float flowRate;
+char buff[10];
 char valueString[20] = "";
 
-char dest[8];
+char dest[30];
 
 
+unsigned int flowMilliLitres;
+unsigned long totalMilliLitres;
 
 
+/*
+//For lolin V3
+void ICACHE_RAM_ATTR pulseCounter()
+{
+  pulseCount++;
+}
+*/
+
+
+// for current esp8266 --> working
+void IRAM_ATTR pulseCounter()
+{
+  pulseCount++;
+}
 
 
 void setup() {
   // put your setup code here, to run once:
-
-    pinMode(SENSORPOWER, OUTPUT);
-  
-    // Initially keep the sensor OFF
-    digitalWrite(SENSORPOWER, LOW);
-
-  
    Serial.begin(115200);
-   WiFi.begin(SSID, SSID_PASSWORD);
+  //thing.add_wifi(SSID, SSID_PASSWORD);
+  WiFi.begin(SSID, SSID_PASSWORD);
 
-    // Connecting to WiFi...
-    Serial.println();
-    Serial.print("Connecting to... ");
-    Serial.println(SSID);
-    while (WiFi.status() != WL_CONNECTED)
-      {
-      delay(4000);
-      Serial.println(".");
-      }
+
+
+
+  // Connecting to WiFi...
+  Serial.print("Connecting to... ");
+  Serial.println(SSID);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(4000);
+    Serial.println(".");
+  }
  
-    // Connected to WiFi
-    Serial.println();
-    Serial.println("Connected! IP address: ");
-    Serial.println(WiFi.localIP());
+  // Connected to WiFi
+  Serial.println();
+  Serial.println("Connected! IP address: ");
+  Serial.println(WiFi.localIP());
 
+
+   
+  
+  pinMode(SENSOR, INPUT_PULLUP);
+  //pinMode(SENSOR, INPUT);
+  pulseCount = 0;
+  flowRate = 0.0;
+  flowMilliLitres = 0;
+  totalMilliLitres = 0;
+  previousMillis = 0;
+  attachInterrupt(digitalPinToInterrupt(SENSOR), pulseCounter, FALLING);//Working
+  //attachInterrupt(SENSOR, pulseCounter, FALLING);//Ubah (NOT NEEDED)
   
 
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    //get the reading from the function below and print it
-  //get the reading from the function below and print it
-  int moisture = readSensor();
-  Serial.print("Analog Output: ");
-  Serial.println(moisture);
-
-  // Determine status of our soil
-  if (moisture < soilWet) {
-    Serial.println("Status: Soil is too wet");
-  } else if (moisture >= soilWet && moisture < soilDry) {
-    Serial.println("Status: Soil moisture is perfect");
-  } else {
-    Serial.println("Status: Soil is too dry - time to water!");
-  }
-  
-  delay(6000);  // Take a reading every second for testing
-          // Normally you should take reading perhaps once or twice a day
-  Serial.println();
+  // put your main code here, to run repeatedly:
+   currentMillis = millis();
+   
+  if (currentMillis - previousMillis > interval) {
+    //detachInterrupt(SENSOR);//Tambah (NOT NEEDED)
+    pulse1Sec = pulseCount;
+    pulseCount = 0;
+    flowRate = ((1000.0 / (millis() - previousMillis)) * pulse1Sec) / calibrationFactor;
+    previousMillis = millis();
+    flowMilliLitres = (flowRate / 60) * 1000;
+    totalMilliLitres += flowMilliLitres;
     
+    // Print the flow rate for this second in litres / minute
+    Serial.print("Flow rate: ");
+    //Serial.print(int(flowRate));  // Print the integer part of the variable
+    Serial.print(flowRate);
+    Serial.println("L/min");
+    //Serial.println();
+    //Serial.print("\t");       // Print tab space
 
     //to send notification to IFTTT
-    if(moisture>soilDry){
+    if(flowRate>30){ //Please adjust this
+
+      //buff[10]="";
+      //valueString = "";
+
+      //dest="";
 
       // Send Webook to IFTTT
-      dtostrf(moisture, 4, 2, buff);  //4 is mininum width, 6 is precision
-      //strcat(valueString, buff);
+      dtostrf(flowRate, 4, 2, buff);  //4 is mininum width, 6 is precision
+      //strcat(valueString, buff); --> I don't need this
       //strcat(valueString, ", ");
       strcpy( dest, buff );
-      Serial.print("moisture: ");
-      Serial.println(moisture);
-      Serial.print("buff: ");
-      Serial.println(buff);
-      Serial.print("dest: ");
-      Serial.println(dest);
       //strcat( dest, "L/min" );
-      send_webhook(IFTTT_Event,IFTTT_Key,"Soil moisture is too dry at:",dest,"Irrigation System: TRIGERRED");
-      //send_webhook(IFTTT_Event,IFTTT_Key,"Taufik, Water is flowing at Left/Short Arm",strcat( dest, "L/min" ),"now");
-      Serial.println("Enter Webhook");
-      delay(10000);//To send notification every 2 minutes
-      
+      send_webhook(IFTTT_Event,IFTTT_Key,"Taufik, Water is flowing at Left/Short Arm",strcat( dest, "L/min" ),"now");
+      //delay(120000);//To send notification every 2 minutes --> please adjust this
+      //Check strcat & strcpy! concatentate look awkward - DONE.
       }
     
-
+    // Print the cumulative total of litres flowed since starting
+    Serial.print("Output Liquid Quantity: ");
+    Serial.print(totalMilliLitres);
+    Serial.print("mL / ");
+    Serial.print(totalMilliLitres / 1000);
+    Serial.println("L");
+    Serial.println();
+    //delay(500);
     
-    thing["data1"] >> [](pson& out){
-      out["Soil moisture"] = readSensor();
-      
+    thing["data"] >> [](pson& out){
+      out["Flow Rate"] = flowRate;
+      out["Total"]= totalMilliLitres;
       };
       
     thing.handle();
-    thing.stream(thing["dat1a"]);
+    thing.stream(thing["data"]);
+    //attachInterrupt(SENSOR, pulseCounter, FALLING);//Tambah (NOT NEEDED)
   }
 
-      //  This function returns the analog soil moisture measurement
-int readSensor() {
-  digitalWrite(SENSORPOWER, HIGH);  // Turn the sensor ON
-  delay(10);              // Allow power to settle
-  int val = analogRead(SENSORPIN);  // Read the analog value form sensor
-  digitalWrite(SENSORPOWER, LOW);   // Turn the sensor OFF
-  return val;             // Return analog moisture value
 }
